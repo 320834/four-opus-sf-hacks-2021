b668415948f9412f57e128007513d40d304ef266
seungguini <sl6369@nyu.edu>
2021-03-07 21:22:07 +0900

    remove bootstrap

--- maybel/src/components/connect.jsx
+++ maybel/src/components/connect.jsx
@@ -1,95 +1,95 @@
-import React from 'react';
-import BASE_URI from '../misc/setup';
-import logo from '../misc/sendIcon.png'
+import React from "react";
+import BASE_URI from "../misc/setup";
+import logo from "../misc/sendIcon.png";
 
-import './styles/connect.css';
+import "./styles/connect.css";
 
 export default class Connect extends React.Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
     this.state = {
-      text: '',
-      score: '',
-      quote: '',
+      text: "",
+      score: "",
+      quote: "",
     };
   }
 
   styles = {
     width: 600,
-    borderColor: 'transparent',
-    defaultValue: 'Hmm..',
+    borderColor: "transparent",
+    defaultValue: "Hmm..",
     fontSize: 50,
-    backgroundColor: 'transparent',
-    outline: 'none',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    display: 'block',
-    resize: 'none',
-    color: 'white',
-    whiteSpace: 'nowrap',
-    textAlign: 'center',
+    backgroundColor: "transparent",
+    outline: "none",
+    marginLeft: "auto",
+    marginRight: "auto",
+    display: "block",
+    resize: "none",
+    color: "white",
+    whiteSpace: "nowrap",
+    textAlign: "center",
     marginBottom: 20,
   };
 
   buttonStyle = {
-    borderColor: 'transparent',
+    borderColor: "transparent",
     fontSize: 20,
-    textTransform: 'uppercase',
-    backgroundColor: 'transparent',
-    fontColor: 'white',
-    outline: 'none',
+    textTransform: "uppercase",
+    backgroundColor: "transparent",
+    fontColor: "white",
+    outline: "none",
   };
 
-  enterKey = event => {
-      if (event.keyCode == 13 && event.shiftKey == false){
-          event.preventDefault();
-          this.getSentiment();
-      }
-  }
+  enterKey = (event) => {
+    if (event.keyCode == 13 && event.shiftKey == false) {
+      event.preventDefault();
+      this.getSentiment();
+    }
+  };
   getSentiment = async () => {
-    let myHeaders = new Headers ();
-    myHeaders.append ('content-type', 'application/json');
-    myHeaders.append ('Access-Control-Allow-Origin', 'localhost:5000');
+    let myHeaders = new Headers();
+    myHeaders.append("content-type", "application/json");
+    myHeaders.append("Access-Control-Allow-Origin", "localhost:5000");
 
-    let raw = JSON.stringify ({text: this.state.text});
+    let raw = JSON.stringify({ text: this.state.text });
 
     let requestOptions = {
-      method: 'POST',
+      method: "POST",
       headers: myHeaders,
       body: raw,
     };
 
     //BASE_URI defined in misc/setup.js
-    fetch (`${BASE_URI}/sentiment-json`, requestOptions)
-      .then (res => res.json ())
-      .then (data => {        
-        console.log (data);
-        this.setState ({score: data['score'], quote: data['quote']});
+    fetch(`${BASE_URI}/sentiment-json`, requestOptions)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ score: data["score"], quote: data["quote"] });
       })
-      .catch (error => {
-        console.log (error);
+      .catch((error) => {
+        console.log(error);
       });
   };
 
-  render () {
+  render() {
     return (
       <div className="connect-root">
         <div>
-          <h4 style={{paddingTop: '50vh', color: 'rgba(2, 190, 165, 1)'}}>Tell us about your day</h4>
+          <h4 style={{ paddingTop: "50vh", color: "rgba(2, 190, 165, 1)" }}>
+            Tell us about your day
+          </h4>
           <input
             type="text"
             placeholder="how am I feeling..?"
             onKeyDown={this.enterKey}
-            onChange={event => {
-              this.setState ({text: event.target.value});
-              
+            onChange={(event) => {
+              this.setState({ text: event.target.value });
             }}
             name="name"
             style={this.styles}
-            
           />
-          <button onClick={this.getSentiment} type = 'button' className= 'btn btn-light'>
-            <img src = {logo} alt="Send" width = '22' height = '22' padding = '-10' />
+          <button onClick={this.getSentiment} type="button">
+            <img src={logo} alt="Send" width="22" height="22" padding="-10" />
           </button>
           <div>{this.state.quote}</div>
         </div>
