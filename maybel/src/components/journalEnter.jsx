7a896be59f97308cd8133c65812e34afb6fecf53
icrews <icrews@iwu.edu>
2021-03-07 03:40:19 -0600

    Added Journal Entry Component (Zay)

--- maybel/src/components/journalEnter.jsx
+++ maybel/src/components/journalEnter.jsx
@@ -1,44 +1,88 @@
 import React, {Component} from 'react';
-import autosize from './autosize'
 
 class JournalEnter extends Component {
-    componentDidMount(){
-        this.textarea.focus();
-        autosize(this.textarea);
-    }
   state = {
-      textInput: 'Hmm..',
+    chatbox: '',
+    value: ''
   };
+
   styles = {
+    width: 600,
     borderColor: 'transparent',
     defaultValue: 'Hmm..',
-    fontSize: 72,
+    fontSize: 50,
     backgroundColor: 'transparent',
     outline: 'none',
     marginLeft: 'auto',
     marginRight: 'auto',
     display: 'block',
     resize: 'none',
-    whiteSpace: 'nowrap'
-
+    whiteSpace: 'nowrap',
   };
 
-  handleUpdate = () => {
-      this.setState({textIn: this.state.textInput});
-  }
+  readOnlyStyles = {
+    width: 600,
+    borderColor: 'transparent',
+    defaultValue: 'Hmm..',
+    fontSize: 30,
+    backgroundColor: 'transparent',
+    outline: 'none',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    display: 'block',
+    resize: 'none',
+    whiteSpace: 'nowrap',
+  };
+
+  handleUpdate = event => {
+    this.setState({value: event.target.value});
+  };
+
+  handleEnter = event => {
+    if (event.keyCode == 13 && event.shiftKey == false) {
+      event.preventDefault();
+      //this.setState({chatbox: this.chatbox + event.target.value});
+      this.setState({value: ''});
+    }
+  };
+
+  /* 
+  handleSubmit = event =>{
+    event.preventDefault();
+    Actions.sendMessage(this.state.value);
+  };
+  */
+
+  /*
+  updateChat = event => {
+    this.setState({value: event.target.chatbox});
+  };
+  */
   
+  
+
   render () {
-    
     return (
-        
       <div>
-        <label >Hello. How are you feeling today?</label>
         <br />
         <textarea
-          style= {this.styles}
-          ref = {c => this.textarea = c}
-          onChange = {this.handleUpdate}
-        ></textarea>
+          style={this.readOnlyStyles}
+          ref={c => (this.textarea = c)}
+          onChange={this.updateChat}
+          readOnly
+          id="chat"
+        />
+        <br />
+        <label>Hello. How are you feeling today?</label>
+        <form ref = {el => this.myFormRef = el} className = '' onSubmit = {this.handleSubmit}>
+          <textarea
+            style={this.styles}
+            ref={c => (this.textarea = c)}
+            onChange = {this.handleUpdate}
+            onKeyDown = {this.handleEnter}
+            id="input area"
+          />
+        </form>
       </div>
     );
   }
