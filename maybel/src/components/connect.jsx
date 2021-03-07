104c5f99cf4402d12a3752f1cf6010b3cacf9fd7
icrews <icrews@iwu.edu>
2021-03-07 05:59:22 -0600

    Beautified by Zay <3
    Centered Invisible Textarea,
    Made Green,
    Added Roboto font (unused, but available),
    Imported bootstrap

--- maybel/src/components/connect.jsx
+++ maybel/src/components/connect.jsx
@@ -1,55 +1,99 @@
 import React from 'react';
-import BASE_URI from "../misc/setup";
+import BASE_URI from '../misc/setup';
+import logo from '../misc/sendIcon.png'
 
-import "./styles/connect.css"
+import './styles/connect.css';
 
 export default class Connect extends React.Component {
-    constructor(props)
-    {
-        super(props)
-        this.state = {
-            text: "",
-            score: "",
-            quote: ""
-        }
-    }
+  constructor (props) {
+    super (props);
+    this.state = {
+      text: '',
+      score: '',
+      quote: '',
+    };
+  }
 
-    getSentiment = async () => {
+  styles = {
+    width: 600,
+    borderColor: 'transparent',
+    defaultValue: 'Hmm..',
+    fontSize: 50,
+    backgroundColor: 'transparent',
+    outline: 'none',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    display: 'block',
+    resize: 'none',
+    color: 'white',
+    whiteSpace: 'nowrap',
+    textAlign: 'center',
+    marginBottom: 20,
+  };
 
-        let myHeaders = new Headers();
-        myHeaders.append("content-type", "application/json");
-        myHeaders.append("Access-Control-Allow-Origin", "localhost:5000");
-    
-        let raw = JSON.stringify({"text": this.state.text});
-    
-        let requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: raw,
-        };
+  buttonStyle = {
+    borderColor: 'transparent',
+    fontSize: 20,
+    textTransform: 'uppercase',
+    backgroundColor: 'transparent',
+    fontColor: 'white',
+    outline: 'none',
+  };
 
-        //BASE_URI defined in misc/setup.js
-        fetch(`${BASE_URI}/sentiment-json`, requestOptions)
-          .then(res => res.json())
-          .then((data) => {
-            console.log(data)
-            this.setState({score: data["score"], quote: data["quote"]})
-          })
-          .catch((error) => {
-              console.log(error)
-          })
-    
-    }
+  enterKey = event => {
+      if (event.keyCode == 13 && event.shiftKey == false){
+          event.preventDefault();
+          this.getSentiment();
+      }
+  }
+  getSentiment = async () => {
+    let myHeaders = new Headers ();
+    myHeaders.append ('content-type', 'application/json');
+    myHeaders.append ('Access-Control-Allow-Origin', 'localhost:5000');
 
-    render()
-    {
-        return(<div className="connect-root">
-            <div>
-                <h3>Tell us more about your day</h3>
-                <input type="text" onChange={(event) => {this.setState({text: event.target.value})}} name="name" />
-                <button onClick={this.getSentiment}>Submit</button>
-                <div>{this.state.quote}</div>
-            </div>
-        </div>)
-    }
+    let raw = JSON.stringify ({text: this.state.text});
+
+    let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+    };
+
+    //BASE_URI defined in misc/setup.js
+    fetch (`${BASE_URI}/sentiment-json`, requestOptions)
+      .then (res => res.json ())
+      .then (data => {        
+        console.log (data);
+        this.setState ({score: data['score'], quote: data['quote']});
+      })
+      .catch (error => {
+        console.log (error);
+      });
+  };
+
+  render () {
+    return (
+      <div className="connect-root">
+        <div>
+          <h4 style={{paddingTop: '50vh', color: 'rgba(2, 190, 165, 1)'}}>Tell us about your day</h4>
+          <input
+            type="text"
+            placeholder="how am I feeling..?"
+            onKeyDown={this.enterKey}
+            onChange={event => {
+              this.setState ({text: event.target.value});
+              
+            }}
+            name="name"
+            style={this.styles}
+            
+          />
+          <button onClick={this.getSentiment} type = 'button' className= 'btn btn-light'>
+            <img src = {logo} alt="Send" width = '22' height = '22' padding = '-10' />
+          </button>
+          <div>{this.state.quote}</div>
+        </div>
+      </div>
+    );
+  }
 }
