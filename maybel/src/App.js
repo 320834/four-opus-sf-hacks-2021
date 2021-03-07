00bbba118d1b80fa9021348bbc13fa9b0eebd943
seungguini <sl6369@nyu.edu>
2021-03-07 21:34:00 +0900

    remove Journal Component

--- maybel/src/App.js
+++ maybel/src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Connect from "./components/connect";
 import Music from "./components/Music";
-import JournalEnter from './components/journalEnter';
-  
+import JournalEnter from "./components/journalEnter";
+
 class App extends React.Component {
   render() {
     return (
       <div class="app">
         <Music></Music>
         <Connect></Connect>
-        <React.Fragment>
-          <JournalEnter />
-        </React.Fragment>
       </div>
     );
   }
